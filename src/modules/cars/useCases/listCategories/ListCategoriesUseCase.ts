1c22d2503d8d5a26ea803fb7d3dd6fa07ffe0292
gabrielodeveloper <gabriel0211oliveira15@gmail.com>
2022-12-20 11:06:40 -0300

    backend rentx

---
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -0,0 +1,14 @@
+import { Category } from "../../model/Category";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+class ListCategoriesUseCase {
+  constructor(private categoriesRepository: ICategoriesRepository) {}
+
+  excute(): Category[] {
+    const categories = this.categoriesRepository.list();
+
+    return categories;
+  }
+}
+
+export { ListCategoriesUseCase };
